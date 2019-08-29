0230540fe0e7a30b7f6351d032c8b2213b5e31f7
killa <ivan_bodny@hotmail.com>
2019-08-28 22:04:05 -0300

    hecho console log de usuario

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+function chargeData(){
+ 
+    var   usuario = {  
+        name: document.getElementById("name").value,
+        password: document.getElementById("password").value
+    };
+
+    console.log("usuario: " , usuario);
+}
+
+
+
+
