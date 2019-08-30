5f0a0d3fca18abb4e92ad0c0afbaefba452b44e4
killa <ivan_bodny@hotmail.com>
2019-08-30 06:55:27 -0300

    variable sexo obtenida y agregada a usuario

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ function chargeData(){
  
     var   usuario = {  
         name: document.getElementById("name").value,
-        password: document.getElementById("password").value
-    };
+        password: document.getElementById("password").value,
+        sexo: obtenerSexo(),
 
+
+    }
     console.log("usuario: " , usuario);
 }
 
-
+function obtenerSexo(){
+    var sexo= document.getElementsByName("sexo");
+    for(var i=0;i<sexo.length;i++){
+        if(sexo[i].checked){ 
+            return sexo[i].value;
+        }
+    }
+}
 
 
